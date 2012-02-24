3be4bd4bac55a01d9eaaae4724146d97796fc4f7
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2012-02-25 01:27:38 +0200

    receives systray opcodes

--- xsystray.c
+++ xsystray.c
@@ -14,26 +14,34 @@ main(int argc, char **argv)
 	Window window;
 	GC gc;
 	XEvent e;
-	Atom atom;
+	Atom sel_atom, opcode_atom;
 
 	dpy = XOpenDisplay(NULL);
 	if (dpy == NULL)
 		errx(1, "failed to open X display");
 
-	atom = XInternAtom(dpy, "_NET_SYSTEM_TRAY_S0", False);
-	window = XGetSelectionOwner(dpy, atom);
-	if (window != None) {
-		fprintf(stderr, "systray selection ownership already taken");
+	sel_atom = XInternAtom(dpy, "_NET_SYSTEM_TRAY_S0", False);
+
+	if (XGetSelectionOwner(dpy, sel_atom) != None) {
+		fprintf(stderr, "systray selection ownership already taken\n");
 		exit(1);
 	}
-
 	window = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, 1, 1, 0,
 	    BlackPixel(dpy, DefaultScreen(dpy)),
 	    WhitePixel(dpy, DefaultScreen(dpy)));
+	XSelectInput(dpy, window, StructureNotifyMask | PropertyChangeMask);
+	XSetSelectionOwner(dpy, sel_atom, window, CurrentTime);
 
-	/* ... */
+	opcode_atom = XInternAtom(dpy, "_NET_SYSTEM_TRAY_OPCODE", False);
+	for ( ;; ) {
+		XNextEvent(dpy, &e);
 
-	XFlush(dpy);
+		if (e.type == ClientMessage
+		    && e.xclient.message_type == opcode_atom
+		    && e.xclient.format == 32) {
+			printf("received systray opcode\n");
+		}
+	}
 
 	XCloseDisplay(dpy);
 
