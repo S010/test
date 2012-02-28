922e44a1c9266c12fc509b6cd94e72e1ce3e1f61
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2012-02-28 22:10:59 +0200

    xsystray dev

--- xsystray.c
+++ xsystray.c
@@ -7,33 +7,91 @@
 
 #include <X11/Xlib.h>
 
+extern char *optarg;
 
+void usage(void);
+int xsystray(int win_x, int win_y, int vertical_layout, int opposite_grow_dir,
+    int icon_size);
+Atom get_systray_selection_atom(Display *dpy);
 
 int
 main(int argc, char **argv)
 {
+	int vertical_layout = 0;
+	int opposite_grow_dir = 0; /* in vertical layout: grow up;
+				  in horizontal layout: grow left;
+				 */
+	int icon_size = 22;
+	int x = 0, y = 0;
+	int ch;
+
+	while ((ch = getopt(argc, argv, "hvosp")) != -1) {
+		switch (ch) {
+		case 'v':
+			++vertical_layout;
+			break;
+		case 'o':
+			++opposite_grow_dir;
+			break;
+		case 's':
+			icon_size = atoi(optarg);
+			break;
+		case 'p':
+			if (sscanf(optarg, "%dx%d", &x, &y) == 0) {
+				usage();
+				return 1;
+			}
+			break;
+		case 'h':
+		default:
+			usage();
+			return 1;
+			break;
+		}
+	}
+
+	return xsystray(vertical_layout, opposite_grow_dir, icon_size);
+
+
+}
+
+void
+usage(void)
+{
+	printf("usage: xsystray [-v] [-o] [-s <int>] [-p <int>x<int>]\n");
+}
+
+int
+xsystray(int tray_win_x, int tray_win_y, int vertical_layout,
+    int opposite_grow_dir, int icon_size)
+{
 	Display *dpy;
-	Window window;
+	Window tray_window, sel_window;
 	GC gc;
 	XEvent e;
-	Atom sel_atom, opcode_atom;
+	Atom selection_atom, opcode_atom;
 
 	enum systray_opcodes {
-		SYSTRAY_OPCODE_DOCK,
-		SYSTRAY_OPCODE_BEGIN_MSG,
-		SYSTRAY_OPCODE_CANCEL_MSG,
+		SYSTEM_TRAY_REQUEST_DOCK,
+		SYSTEM_TRAY_BEGIN_MESSAGE,
+		SYSTEM_TRAY_CANCEL_MESSAGE,
 	};
 
 	dpy = XOpenDisplay(NULL);
 	if (dpy == NULL)
 		errx(1, "failed to open X display");
 
-	sel_atom = XInternAtom(dpy, "_NET_SYSTEM_TRAY_S0", False);
+	selection_atom = get_selection_atom(dpy);
+	tray_window = open_tray_window(dpy, tray_win_x, tray_win_y);
+	if (acquire_systray_selection(Display *dpy, Atom selection_atom))
+		errx("failed to acquire ownership of systray selection");
 
+	sel_atom = XInternAtom(dpy, "_NET_SYSTEM_TRAY_S0", False);
 	if (XGetSelectionOwner(dpy, sel_atom) != None) {
 		fprintf(stderr, "systray selection ownership already taken\n");
 		exit(1);
 	}
+
 	window = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, 1, 1, 0,
 	    BlackPixel(dpy, DefaultScreen(dpy)),
 	    WhitePixel(dpy, DefaultScreen(dpy)));
@@ -77,4 +135,39 @@ main(int argc, char **argv)
 	XCloseDisplay(dpy);
 
 	return 0;
+}
+
+Atom
+get_systray_selection_atom(Display *dpy)
+{
+	char buf[sizeof("_NET_SYSTEM_TRAY_Sxxx")];
+	size_t bufsize = sizeof(buf);
+	Atom atom;
+
+	snprintf(buf, bufsize, "_NET_SYSTEM_TRAY_S%d", DefaultScreen(dpy));
+	atom = XInternAtom(dpy, buf, False);
+
+	return atom;
+}
+
+int
+acquire_systray_selection(Display *dpy, Atom selection_atom)
+{
+	if (XGetSelectionOwner(dpy, selection_atom) != None)
+		return -1;
+	XSetSelectionOwner(dpy, selection_atom, 
+
+}
+
+Window
+open_tray_window(Display *dpy, int x, int y)
+{
+	Window window;
+
+	window = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), x, y, 1, 1, 0,
+	    BlackPixel(dpy, DefaultScreen(dpy)),
+	    WhitePixel(dpy, DefaultScreen(dpy)));
+	XSelectInput(dpy, window, StructureNotifyMask | PropertyChangeMask);
+
+	return window;
 }
