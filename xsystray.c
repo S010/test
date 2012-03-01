80c5e5b5c3b0d1cf042658a2c0eb09c019ee07ab
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2012-03-02 01:34:28 +0200

    xsystray dev

--- xsystray.c
+++ xsystray.c
@@ -8,44 +8,29 @@
 #include <X11/Xlib.h>
 
 static void	 usage(void);
-static int	 xsystray(int, int, int, int, int);
+static int	 xsystray(int);
 static Atom	 create_tray_atom(Display *);
-static int	 acquire_systray_selection(Display *, Atom, Window);
-static Window	 open_tray_window(Display *, int, int);
-static Window	 open_sel_owner(Display *, Window);
+static void	 acquire_systray_selection(Display *, Atom, Window);
+static Window	 open_tray_window(Display *, int);
 static void	 notify_systray_appeared(Display *, Atom, Window);
 static int	 same_screen(Display *, Window);
 static void	 xembed_notify_embedded(Display *, Window, Window);
-static void	 update_geometry(Display *, Window);
-static void	 arrange_children(Display *, Window);
-static void	 show_tray(Display *, Window);
+static void	 update_geometry(Display *, Window, int);
+static void	 arrange_children(Display *, Window, int);
+static int	 get_n_children(Display *, Window);
 
 int
 main(int argc, char **argv)
 {
 	extern char	*optarg;
-	int		 vertical_layout = 0;
-	int		 opposite_grow_dir = 0;
-	int		 icon_size = 22;
-	int		 x = 0, y = 0;
+	int		 icon_size = 24;
 	int		 ch;
+	int		 rc;
 
 	while ((ch = getopt(argc, argv, "hvosp")) != -1) {
 		switch (ch) {
-		case 'v':
-			++vertical_layout;
-			break;
-		case 'o':
-			++opposite_grow_dir;
-			break;
 		case 's':
 			icon_size = atoi(optarg);
-			break;
-		case 'p':
-			if (sscanf(optarg, "%dx%d", &x, &y) == 0) {
-				usage();
-				return 1;
-			}
 			break;
 		case 'h':
 		default:
@@ -55,22 +40,24 @@ main(int argc, char **argv)
 		}
 	}
 
-	return xsystray(x, y, vertical_layout, opposite_grow_dir, icon_size);
+	rc = xsystray(icon_size);
+
+	return rc;
 }
 
 static void
 usage(void)
 {
-	printf("usage: xsystray [-v] [-o] [-s <int>] [-p <int>x<int>]\n");
+	printf("usage: xsystray [-s <int>]\n");
+	printf("  -s -- set the size of the icons (24 by default)\n");
 }
 
 static int
-xsystray(int x, int y, int vertical_layout, int opposite_grow_dir,
-    int icon_size)
+xsystray(int icon_size)
 {
 	Display	*display;
 	Atom	 tray_atom, opcode_atom;
-	Window	 tray, sel_owner, icon;
+	Window	 tray, icon;
 	XEvent	 e;
 	long	 opcode;
 	enum opcodes {
@@ -86,15 +73,19 @@ xsystray(int x, int y, int vertical_layout, int opposite_grow_dir,
 	tray_atom = create_tray_atom(display);
 	opcode_atom = XInternAtom(display, "_NET_SYSTEM_TRAY_OPCODE", False);
 
-	tray = open_tray_window(display, x, y);
-	//XMapWindow(display, tray);
-	sel_owner = open_sel_owner(display, tray);
-	if (acquire_systray_selection(display, tray_atom, sel_owner))
-		errx(1, "failed to acquire ownership of systray selection");
-	notify_systray_appeared(display, tray_atom, sel_owner);
+	tray = open_tray_window(display, icon_size);
+	acquire_systray_selection(display, tray_atom, tray);
+	notify_systray_appeared(display, tray_atom, tray);
 
 	for ( ;; ) {
 		XNextEvent(display, &e);
+
+		printf("XEvent, type=%d\n", (int) e.type);
+
+		if (e.type == DestroyNotify && e.xdestroywindow.window != tray) {
+			arrange_children(display, tray, icon_size);
+			update_geometry(display, tray, icon_size);
+		}
 
 		if (!(e.type == ClientMessage
 		    && e.xclient.message_type == opcode_atom
@@ -109,17 +100,11 @@ xsystray(int x, int y, int vertical_layout, int opposite_grow_dir,
 			if (!same_screen(display, icon))
 				continue;
 			XReparentWindow(display, icon, tray, 0, 0);
-			xembed_notify_embedded(display, sel_owner, icon);
-			/* TODO
-			 * resize to icon_size
-			 * map if unmapped
+			xembed_notify_embedded(display, tray, icon);
+			XResizeWindow(display, icon, icon_size, icon_size);
 			XMapWindow(display, icon);
-			*/
-			arrange_children(display, tray);
-			update_geometry(display, tray);
-			/* TODO
-			 * if we have >0 icons, map the tray window
-			 */
+			arrange_children(display, tray, icon_size);
+			update_geometry(display, tray, icon_size);
 		} else if (opcode == SYSTEM_TRAY_BEGIN_MESSAGE) {
 			printf("system_tray_begin_message\n");
 		} else if (opcode == SYSTEM_TRAY_CANCEL_MESSAGE) {
@@ -145,24 +130,23 @@ create_tray_atom(Display *display)
 	return atom;
 }
 
-static int
+static void
 acquire_systray_selection(Display *display, Atom tray_atom, Window owner)
 {
 	if (XGetSelectionOwner(display, tray_atom) != None)
-		return -1;
+		errx(1, "failed to acquire systray selection ownership");
 	XSetSelectionOwner(display, tray_atom, owner, CurrentTime);
-	return 0;
 }
 
 static Window
-open_tray_window(Display *display, int x, int y)
+open_tray_window(Display *display, int icon_size)
 {
 	Window	 window;
 
 	window = XCreateSimpleWindow(display, DefaultRootWindow(display),
-	    x, y, 40, 40, 0,
-	    BlackPixel(display, DefaultScreen(display)),
-	    WhitePixel(display, DefaultScreen(display)));
+	    0, 0, 1, icon_size, 0,
+	    WhitePixel(display, DefaultScreen(display)),
+	    BlackPixel(display, DefaultScreen(display)));
 
 	XSelectInput(display, window,
 	    ButtonPressMask |
@@ -172,20 +156,7 @@ open_tray_window(Display *display, int x, int y)
 	    SubstructureNotifyMask |
 	    SubstructureRedirectMask);
 
-	return window;
-}
-
-/* open the window that will own the systray selection */
-static Window
-open_sel_owner(Display *display, Window parent)
-{
-	Window	 window;
-
-	window = XCreateSimpleWindow(display, parent, 0, 0, 1, 1, 0,
-	    BlackPixel(display, DefaultScreen(display)),
-	    WhitePixel(display, DefaultScreen(display)));
-
-	XSelectInput(display, window, SubstructureNotifyMask);
+	XMapWindow(display, window);
 
 	return window;
 }
@@ -225,7 +196,7 @@ same_screen(Display *display, Window icon)
 }
 
 static void
-xembed_notify_embedded(Display *display, Window sel_window, Window window)
+xembed_notify_embedded(Display *display, Window embedder, Window embedded)
 {
 	XEvent	 e;
 	enum {
@@ -237,32 +208,56 @@ xembed_notify_embedded(Display *display, Window sel_window, Window window)
 	e.xclient.type = ClientMessage;
 	e.xclient.message_type = XInternAtom(display, "_XEMBED", False);
 	e.xclient.display = display;
-	e.xclient.window = window;
+	e.xclient.window = embedded;
 	e.xclient.format = 32;
 	e.xclient.data.l[0] = CurrentTime;
 	e.xclient.data.l[1] = XEMBED_EMBEDDED_NOTIFY;
 	e.xclient.data.l[2] = 0; /* protocol version */
-	e.xclient.data.l[3] = sel_window;
+	e.xclient.data.l[3] = embedder;
 
-	XSendEvent(display, window, False, NoEventMask, &e);
+	XSendEvent(display, embedded, False, NoEventMask, &e);
 }
 
 static void
-update_geometry(Display *display, Window tray)
+update_geometry(Display *display, Window tray, int icon_size)
 {
+	int	 width;
+
+	width = icon_size * get_n_children(display, tray);
+	if (width < 1)
+		width = 1;
+	XResizeWindow(display, tray, width, icon_size);
+
+	/*
+	 * TODO
+	 * place the window in lower-right corner of the screen
+	 */
 }
 
 static void
-arrange_children(Display *display, Window tray)
+arrange_children(Display *display, Window tray, int icon_size)
 {
+	Window	 root, parent, *icon, *children;
+	int	 x, n_children;
+
+	XQueryTree(display, tray, &root, &parent, &children, &n_children);
+
+	for (icon = children, x = 0; n_children-- > 0; ++icon, x += icon_size)
+		XMoveWindow(display, *icon, x, 0);
+	XFree(children);
 }
 
-static void
-show_tray(Display *display, Window tray)
+static int
+get_n_children(Display *display, Window window)
 {
-	XWindowAttributes	 attrs;
+	Window	 root, parent, *children;
+	int	 n;
 
-	XGetWindowAttributes(display, tray, &attrs);
-	if (attrs.map_state & IsUnmapped)
-		XMapWindow(display, tray);
+	XQueryTree(display, window, &root, &parent, &children, &n);
+
+	if (children != NULL)
+		XFree(children);
+
+	return n;
 }
+
