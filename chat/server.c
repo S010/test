94fa3b163b940681f9073d07a7dbb0f3965625b3
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2013-01-30 12:01:41 +0200

    add

--- chat/server.c
+++ chat/server.c
@@ -8,11 +8,69 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <poll.h>
 
 #define DEFAULT_PORT	1234
+#define POLL_TIMEOUT	-1
 
 void usage(void);
 void server(unsigned short);
+
+struct pfds {
+	struct pollfd	*fds;
+	int		 n;
+};
+
+void
+pfds_init(struct pfds *p)
+{
+	p->fds = NULL;
+	p->n = 0;
+}
+
+int
+pfds_add(struct pfds *p, int fd, short events)
+{
+	struct pollfd *q;
+	if (p->n == INT_MAX) {
+		warnx("pfds_add: maximum number of pollfds reached");
+		return -1;
+	}
+	q = realloc(p->fds, sizeof(*p->fds) * (p->n + 1));
+	if (q == NULL) {
+		warn("pfds_add: realloc");
+		return -1;
+	}
+	p->fds = q;
+	++p->n;
+	return 0;
+}
+
+int
+pfds_del(struct pfds *p, int fd)
+{
+	struct pollfd	*q;
+	struct pollfd	*last = p->fds + n - 1;
+	struct pollfd	 tmp;
+
+	for (struct pollfd *q = p->fds; q <= last; ++q) {
+		if (q->fd != fd)
+			continue;
+		if (q != last) {
+			tmp = *last;
+			*last = *q;
+			*q = tmp;
+		}
+		q = realloc(p->fds, sizeof(*p->fds) * (p->n - 1));
+		if (q != NULL) {
+			warn("pfds_del: realloc");
+			p->fds = q;
+		}
+		--p->n;
+		return 0;
+	}
+	return -1;
+}
 
 int
 main(int argc, char **argv)
@@ -48,18 +106,26 @@ usage(void)
 }
 
 void
+setnonblock(int fd)
+{
+	if (fcntl(fd, F_SETFD, O_NONBLOCK) == -1)
+		err(1, "setnonblock: fcntl");
+}
+
+void
 server(unsigned short port)
 {
 	int			 s;
 	int			 conn;
 	struct sockaddr_in	 sa;
-	char			*buf;
-	const size_t		 bufsize = 8192;
-	size_t			 nread;
+	ssize_t			 nread;
+	struct			 pfds;
+        int			 npoll;
 
 	s = socket(AF_INET, SOCK_STREAM, 0);
 	if (s == -1)
 		err(1, "socket");
+        setnonblock(s);
 	memset(&sa, 0, sizeof sa);
 	sa.sin_family = AF_INET;
 	sa.sin_port = htons(port);
@@ -69,8 +135,11 @@ server(unsigned short port)
 	if (listen(s, 0) == -1)
 		err(1, "listen");
 
-	buf = malloc(bufsize);
-	for ( ;; ) {
+	pfds_init(&pfds);
+	pfds_add(&pfds, s, POLLIN | POLLOUT);
+
+	while ((npoll = poll(pfds.fds, pfds.n, POLL_TIMEOUT)) != -1) {
+		/* // ???
 		conn = accept(s, NULL, NULL);
 		if (conn == -1) {
 			warn("accept");
@@ -80,5 +149,8 @@ server(unsigned short port)
 		while ((nread = read(conn, buf, bufsize)) > 0)
 			write(conn, buf, nread);
 		close(conn);
+		*/
 	}
+	if (npoll < 0)
+		err(1, "server: poll");
 }
---
+++ chat/wbuf.hpp
@@ -0,0 +1,10 @@
+#ifndef WBUF
+#define WBUF
+
+class wbuf {
+    public:
+
+        void write(
+};
+
+#endif
