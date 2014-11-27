5f90ff9d6483b686acb801fe846365f3475e1fd7
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2014-11-27 04:25:56 +0200

    Add tgz

---
+++ tgz/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+type ChanBuf struct {
+	ch	chan []byte
+	buf	[]byte
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func (cb *ChanBuf) Read(buf []byte) (n int, err error) {
+	var ok bool
+	if cb.buf == nil {
+		cb.buf, ok = <-cb.ch
+		if !ok {
+			return 0, io.EOF
+		}
+	}
+	l := copy(buf, cb.buf)
+	cb.buf = cb.buf[l:]
+	if len(cb.buf) == 0 {
+		cb.buf = nil
+	}
+	return l, nil
+}
+
+func (cb *ChanBuf) Write(buf []byte) (n int, err error) {
+	cb.ch <- buf
+	return len(buf), nil
+}
+
+func process(cb *ChanBuf) {
+	buf := make([]byte, 2)
+	for {
+		n, err := cb.Read(buf)
+		if n > 0 {
+			fmt.Printf("n=%v, buf=%v\n", n, buf)
+		}
+		if err != nil {
+			return
+		}
+	}
+}
+
+func main() {
+	fmt.Println("hello, world")
+
+	cb := ChanBuf{make(chan []byte), nil}
+	go process(&cb)
+	cb.ch <- []byte("abc")
+	close(cb.ch)
+}
