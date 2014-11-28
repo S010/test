599338bb6de5077674bb9c818e4cb1f7586a0556
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2014-11-28 04:03:33 +0200

    Works.

--- tgz/main.go
+++ tgz/main.go
@@ -1,89 +1,72 @@
 package main
 
 import (
+	"archive/tar"
+	"compress/gzip"
+	"path/filepath"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
-type ChanBuf struct {
-	ch	chan []byte
-	buf	[]byte
-}
+func tarball(path string, t *tar.Writer) {
+	path = filepath.Clean(path)
 
-func min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func (cb *ChanBuf) Read(buf []byte) (n int, err error) {
-	var ok bool
-	if cb.buf == nil {
-		cb.buf, ok = <-cb.ch
-		if !ok {
-			return 0, io.EOF
-		}
-	}
-	l := copy(buf, cb.buf)
-	cb.buf = cb.buf[l:]
-	if len(cb.buf) == 0 {
-		cb.buf = nil
-	}
-	return l, nil
-}
-
-func (cb *ChanBuf) Write(buf []byte) (n int, err error) {
-	cb.ch <- buf
-	return len(buf), nil
-}
-
-func process(cb *ChanBuf) {
-	buf := make([]byte, 2)
-	for {
-		n, err := cb.Read(buf)
-		if n > 0 {
-			fmt.Printf("n=%v, buf=%v\n", n, buf)
-		}
-		if err != nil {
-			return
-		}
-	}
-}
-
-func tgz(path string, tw *io.Writer) {
-	fi, err := os.Stat(path)
+	info, err := os.Stat(path)
 	if err != nil {
 		return
 	}
 
-	if tw == nil {
-		tw = ChanBuf{make(chan []byte), nil}
-		/*
-		start a go routine which reads from chanbuf and 
-		dumps into specified file
-		*/
+	if t == nil {
+		r, w := io.Pipe()
+
+		t = tar.NewWriter(w)
+
+		f, err := os.Create(fmt.Sprintf("%s.tgz", path))
+		if err != nil {
+			panic(err)
+		}
+		z := gzip.NewWriter(f)
+
+		defer f.Close()
+		defer z.Close()
+		defer r.Close()
+		defer w.Close()
+		defer t.Close()
+
+		go io.Copy(z, r)
 	}
 
-	if fi.IsDir() {
-		fmt.Printf("%s является директорией\n", path)
+	h, err := tar.FileInfoHeader(info, path) // FIXME what is link arg supposed to be?
+	if err != nil {
+		return
+	}
+	h.Name = path
+	t.WriteHeader(h)
+	if info.IsDir() {
+		infos, err := ioutil.ReadDir(path)
+		if err != nil {
+			return
+		}
+		for _, info := range infos {
+			tgz(fmt.Sprintf("%v/%v", path, info.Name()), t)
+		}
 	} else {
-		fmt.Printf("%s не является директорией\n", path)
+		f, err := os.Open(path)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+
+		if _, err := io.Copy(t, f); err != nil {
+			panic(err)
+		}
 	}
 }
 
 func main() {
-	/*
-	fmt.Println("hello, world")
-
-	cb := ChanBuf{make(chan []byte), nil}
-	go process(&cb)
-	cb.ch <- []byte("abc")
-	close(cb.ch)
-	*/
-
 	for _, arg := range os.Args[1:] {
-		tgz(arg, nil)
+		tarball(arg, nil)
 	}
 }
