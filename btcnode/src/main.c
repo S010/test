0887fa8c29f71e09a8725b1966a5722a92c1a103
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2017-03-13 01:25:23 +0000

    Add skeleton code for handling peers

--- btcnode/src/main.c
+++ btcnode/src/main.c
@@ -72,6 +72,7 @@ struct peer {
 		PEER_VERACK,
 		PEER_CONNECTED
 	} state;
+	int addr_family;
 	struct sockaddr_in6 addr;
 	struct version_msg desc;
 	struct protocol proto;
@@ -111,6 +112,7 @@ xmalloc(size_t size)
 	return ptr;
 }
 
+#if 0
 struct addrinfo *
 discover_peers(void)
 {
@@ -144,6 +146,72 @@ discover_peers(void)
 	}
 
 	return addrs;
+}
+#endif
+
+void
+hexdump(const void *ptr, size_t size)
+{
+	const uint8_t *buf = ptr;
+	size_t i;
+	for (i = 0; i < size; ++i) {
+		if (i > 0 && (i % 16) == 0)
+			putchar('\n');
+		else if (i > 0 && (i % 8) == 0)
+			printf("  ");
+		else if (i > 0 && (i % 4) == 0)
+			putchar(' ');
+		if (i == 0 || (i % 16) == 0)
+			printf("0x%08lx| ", i);
+		printf("%02x", buf[i]);
+	}
+	if ((i % 16) == 0)
+		putchar('\n');
+}
+
+struct peer *
+discover_peers(void)
+{
+	const char *domain_name = "seed.bitcoin.sipa.be.";
+
+	struct addrinfo *addrs = NULL;
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_socktype = SOCK_STREAM;
+	if (getaddrinfo(domain_name, NULL, &hints, &addrs) == -1) {
+		perror("getaddrinfo");
+		return NULL;
+	}
+
+	printf("Discovering peers:\n");
+	struct peer *head = NULL;
+	struct peer *peer = NULL;
+	for (struct addrinfo *ai = addrs; ai != NULL; ai = ai->ai_next) {
+		switch (ai->ai_family) {
+		case AF_INET:
+		case AF_INET6:
+			peer = xcalloc(1, sizeof(*peer));
+			peer->addr_family = ai->ai_family;
+			if (ai->ai_family == AF_INET6) {
+				peer->addr = *((struct sockaddr_in6 *)ai->ai_addr);
+			} else {
+				uint8_t *s6_addr_ptr = peer->addr.sin6_addr.s6_addr;
+				void *s_addr_ptr = &((struct sockaddr_in *)ai->ai_addr)->sin_addr.s_addr;
+				memset(s6_addr_ptr + 10, 0xff, 2);
+				memcpy(s6_addr_ptr + 12, s_addr_ptr, 4);
+				hexdump(s6_addr_ptr, 16);
+			}
+			peer->next = head;
+			head = peer;
+			break;
+		default:
+			continue;
+		}
+
+	}
+	freeaddrinfo(addrs);
+
+	return head;
 }
 
 void
@@ -223,6 +291,139 @@ probe_peer(int family, const struct sockaddr *sa, socklen_t salen)
 	say_hello(family, sa, salen, peer);
 }
 
+void
+print_peers(struct peer *peer)
+{
+	void *addr;
+	char addrstr[INET6_ADDRSTRLEN];
+	while (peer != NULL) {
+		switch (peer->addr_family) {
+		case AF_INET:
+			addr = peer->addr.sin6_addr.s6_addr + 12;
+			break;
+		case AF_INET6:
+			addr = &peer->addr.sin6_addr;
+			break;
+		default:
+			printf("(unknown address family)\n");
+			continue;
+		}
+		printf("%s\n", inet_ntop(peer->addr_family, addr, addrstr, sizeof(addrstr)));
+		peer = peer->next;
+	}
+}
+
+int
+peer_connect(struct peer *peer)
+{
+	int family;
+	struct sockaddr_in sin;
+	struct sockaddr_in6 sin6;
+	struct sockaddr *sa;
+	socklen_t sa_len;
+	int conn;
+
+	family = peer->addr_family;
+
+	memset(&sin, 0, sizeof(sin));
+	memset(&sin6, 0, sizeof(sin6));
+
+	switch (family) {
+	case AF_INET:
+		sin.sin_family = AF_INET;
+		sin.sin_port = htons(MAINNET_PORT);
+		memcpy(peer->addr.sin6_addr.s6_addr + 12, &sin.sin_addr.s_addr, 4);
+		sa = (struct sockaddr *)&sin;
+		sa_len = sizeof(sin);
+		break;
+	case AF_INET6:
+		sin6 = peer->addr;
+		sin6.sin6_port = htons(MAINNET_PORT);
+		sa = (struct sockaddr *)&sin6;
+		sa_len = sizeof(sin6);
+		break;
+	default:
+		syslog(LOG_ERR, "%s(): unknown address family %d", __func__, family);
+		return -1;
+	}
+
+	conn = socket(family, SOCK_STREAM, 0);
+	if (conn == -1) {
+		syslog(LOG_WARNING, "%s(): failed to create stream socket for address family %d, errno %d",
+		    __func__, family, errno);
+		return errno;
+	}
+
+
+	char addrstr[INET6_ADDRSTRLEN];
+	inet_ntop(family, family == AF_INET ? (void *)&sin.sin_addr : (void *)&sin6.sin6_addr,
+	    addrstr, sizeof(addrstr));
+
+	syslog(LOG_DEBUG, "%s(): connecting to %s", __func__, addrstr);
+	if (connect(conn, sa, sa_len) == -1) {
+		syslog(LOG_WARNING, "%s(): failed to connect to %s", __func__, addrstr);
+		close(conn);
+		return -1;
+	}
+
+	peer->state = PEER_VERSION;
+	peer->proto.conn = conn;
+	peer->proto.state = PROTOCOL_IDLE;
+
+	return 0;
+}
+
+void *
+peer_free_list(struct peer *peer)
+{
+	struct peer *next;
+	while (peer != NULL) {
+		next = peer->next;
+		if (peer->proto.conn != -1)
+			close(peer->proto.conn);
+		free(peer->proto.buf.ptr);
+		free(peer);
+		peer = next;
+	}
+	return NULL;
+}
+
+#if 0
+int
+peer_exchange_versions(struct peer *peer)
+{
+	return -1;
+}
+
+int
+peer_exchange_verack(struct peer *peer)
+{
+	return -1;
+}
+
+int
+peer_recv_msg(struct peer *peer)
+{
+	return -1;
+}
+
+int
+peer_advance_state(struct peer *peer)
+{
+	switch (peer->state) {
+	case PEER_VERSION:
+		return peer_exchange_versions(peer);
+	case PEER_VERACK:
+		return peer_exchange_verack(peer);
+	case PEER_CONNECTED:
+		return peer_recv_msg(peer);
+	default:
+		syslog(LOG_CRIT, "%s(): peer is in unknown state %d", __func__, peer->state);
+		return -1;
+	}
+}
+#endif
+
 int
 main(int argc, char **argv)
 {
@@ -233,14 +434,8 @@ main(int argc, char **argv)
 
 	srand(time(NULL) ^ getpid());
 
-	struct addrinfo *addrs = discover_peers();
-	for (struct addrinfo *ai = addrs; ai != NULL; ai = ai->ai_next) {
-		if (ai->ai_family == AF_INET6)
-			continue;
-		probe_peer(ai->ai_family, ai->ai_addr, ai->ai_addrlen);
-		break;
-	}
-	freeaddrinfo(addrs);
+	struct peer *peers = discover_peers();
+	print_peers(peers);
 
 	return 0;
 }
