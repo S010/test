3afed1025c43d0e5b1c776d0c96eed75de4af883
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2018-04-06 02:11:47 +0100

    Add asm/

--- asm/mips32be/hello.s
+++ asm/mips32be/hello.s
@@ -3,26 +3,15 @@
 
 	.globl __start
 __start:
-	.ent __start
-	.frame	$sp, 32, $ra
-	.set noreorder
-	.cpload		$t9
-	.set reorder
-	subu	$sp, $sp, 32
-	sw	$ra, 28($sp)
-	.cprestore	24
-
-	la	$a1, msg
 	li	$v0, write
 	li	$a0, 1
+	la	$a1, msg
 	li	$a2, 14
 	syscall
 
 	li	$v0, exit
 	move	$a0, $v0
 	syscall
-
-	.end __start
 
 
 	.data
